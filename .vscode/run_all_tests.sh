f83a8d8bc052f7cfe1744cbb4b6dded972df9aa4
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2024-04-21 13:53:22 -0300

    Enhance testing

--- .vscode/run_all_tests.sh
+++ .vscode/run_all_tests.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 for i in ./src/*.vhdl; do
-  ghdl -a "$i"
+  ghdl -i "$i"
 done
 
 for i in ./tests/*.vhdl; do
   ENTITY=${i%*.vhdl}
   ENTITY=$(basename $ENTITY)
 
-  echo testing $ENTITY...
 
   mkdir -p wave-files/
 
-  ghdl -a "$i"
-  ghdl -e "$ENTITY"
+  ghdl -i "$i"
+  ghdl -m "$ENTITY"
+  echo testing $ENTITY...
   ghdl -r "$ENTITY" --wave="wave-files/$ENTITY.ghw"
 
   echo done
