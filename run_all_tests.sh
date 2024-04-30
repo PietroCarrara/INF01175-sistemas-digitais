55bf1867009a1569b2769731aedff5bacbed75ff
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2024-04-30 09:46:43 -0300

    Tests can be used in ci now

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
+EXIT_CODE=0 # Success by default
+trap EXIT_CODE=1 ERR # Failure if any command fails
+
 for i in ./src/*.vhdl; do
-  ghdl -i "$i"
+  ENTITY=${i%*.vhdl}
+  ENTITY=$(basename $ENTITY)
+
+  ghdl -i "$i1"
+  ghdl -m "$ENTITY"
+  ghdl --synth --out=none "$ENTITY"
 done
 
 for i in ./tests/*.vhdl; do
   ENTITY=${i%*.vhdl}
   ENTITY=$(basename $ENTITY)
 
-
   mkdir -p wave-files/
 
   ghdl -i "$i"
   ghdl -m "$ENTITY"
   echo testing $ENTITY...
-  ghdl -r "$ENTITY" --wave="wave-files/$ENTITY.ghw"
+  ghdl -r "$ENTITY" --assert-level=error --wave="wave-files/$ENTITY.ghw"
 
   echo done
 done
 
 ghdl remove
+
+exit $EXIT_CODE
